85dab730e7b9a2ec9b4eef791edb3572cee26c15
Rekseto <ReksetoAjx@gmail.com>
2018-07-09 21:47:26 +0200

    Sorting files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./index.css";
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./components/App";
-import registerServiceWorker from "./workers/ServiceWorker";
-ReactDOM.render(<App />, document.getElementById("root"));
-
-registerServiceWorker();
